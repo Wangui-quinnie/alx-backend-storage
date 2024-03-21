f760bf6827efcec3cfe286896a8aa672d153556e
Wangui-quinnie <w.wangui@ymail.com>
2024-03-21 18:03:06 +0000

    added a function

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,17 @@
 -- List all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, 
-       IF(SUBSTRING_INDEX(lifespan, '-', -1) = 'present', 
-          2022 - CAST(SUBSTRING_INDEX(lifespan, '-', 1) AS UNSIGNED),
-          CAST(SUBSTRING_INDEX(lifespan, '-', -1) AS UNSIGNED) - CAST(SUBSTRING_INDEX(lifespan, '-', 1) AS UNSIGNED)) AS lifespan
+-- Function to compute the lifespan based on formed and split attributes
+DROP FUNCTION IF EXISTS lifespan;
+DELIMITER //
+CREATE FUNCTION lifespan(formed INTEGER, split INTEGER)
+    RETURNS INTEGER DETERMINISTIC
+BEGIN
+    RETURN IFNULL(split, 2022) - formed;
+END//
+DELIMITER ;
+
+-- Query to select bands with Glam rock as their main style and compute
+-- their lifespan
+SELECT band_name, lifespan(formed, split) AS lifespan
 FROM metal_bands
 WHERE main_style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
