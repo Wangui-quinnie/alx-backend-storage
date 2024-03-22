86d2f694d4467e4f7264e500beb483fe3d590894
Wangui-quinnie <w.wangui@ymail.com>
2024-03-22 00:19:35 +0000

    changed the use of IFNULL to just NULL

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -5,7 +5,11 @@ DELIMITER //
 CREATE FUNCTION lifespan(formed INTEGER, split INTEGER)
     RETURNS INTEGER DETERMINISTIC
 BEGIN
-    RETURN IFNULL(split, 2022) - formed;
+    IF split IS NULL THEN
+        RETURN 2022 - formed;
+    ELSE
+        RETURN split - formed;
+    END IF;
 END//
 DELIMITER ;
 
@@ -13,5 +17,5 @@ DELIMITER ;
 -- their lifespan
 SELECT band_name, lifespan(formed, split) AS lifespan
 FROM metal_bands
-WHERE main_style LIKE '%Glam rock%'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
