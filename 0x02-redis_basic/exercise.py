02ccbc7a81b4bdf1b143863e1fd642b8ca5b562a
Wangui-quinnie <w.wangui@ymail.com>
2024-03-28 01:35:09 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@ Cache module
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -34,3 +34,47 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> bytes:
+        """
+        Retrieve data from the Redis cache using the provided key.
+
+        Args:
+            key: The key under which the data is stored in the cache.
+            fn: An optional callable to convert the retrieved data back to the
+            desired format.
+
+        Returns:
+            The retrieved data as bytes, or None if the key does not
+            exist in the cache.
+        """
+        data = self._redis.get(key)
+        if fn is not None and data is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve a string value from the Redis cache using the provided key.
+
+        Args:
+            key: The key under which the string value is stored in the cache.
+
+        Returns:
+            The retrieved string value, or None if the key does not exist in
+            the cache.
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve an integer value from the Redis cache using the provided key.
+
+        Args:
+            key: The key under which the integer value is stored in the cache.
+
+        Returns:
+            The retrieved integer value, or None if the key does not exist
+            in the cache.
+        """
+        return self.get(key, fn=int)
